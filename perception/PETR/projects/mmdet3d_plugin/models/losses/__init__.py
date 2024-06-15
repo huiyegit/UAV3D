40d0269809b44d5f841b035ce29827dbbb793f98
hui ye <huiye@huis-MacBook-Pro.local>
2024-06-15 00:08:22 -0400

    Initial commit

--- perception/PETR/projects/mmdet3d_plugin/models/losses/__init__.py
+++
@@ -1,5 +0,0 @@
-
-from .Sigmoid_ce_loss import Sigmoid_ce_loss
-__all__ = [
-    'Sigmoid_ce_loss'
-]
--- perception/detr3d/projects/mmdet3d_plugin/models/detectors/__init__.py
+++
@@ -1,4 +0,0 @@
-from .obj_dgcnn import ObjDGCNN
-from .detr3d import Detr3D
-
-__all__ = [ 'ObjDGCNN', 'Detr3D']
